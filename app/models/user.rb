615b38cc422d60812c25281e692d1eeee41b017f
Oct <f17K6067@kanto-gakuin.ac.jp>
2020-04-27 06:05:38 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ApplicationRecord
     has_secure_password
     
     has_many :microposts
+    
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow # ＜＝ この行
     has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
     has_many :followers, through: :reverses_of_relationship, source: :user # ＜＝ この行
+    
+    has_many :favorites
+    has_many :favoritings, through: :favorites, source: :micropost
     
     def follow(other_user)
         unless self == other_user
@@ -21,7 +25,7 @@ class User < ApplicationRecord
     def unfollow(other_user)
         relationship = self.relationships.find_by(follow_id: other_user.id)
         relationship.destroy if relationship
-     end
+    end
 
     def following?(other_user)
         self.followings.include?(other_user)
@@ -29,5 +33,18 @@ class User < ApplicationRecord
     
     def feed_microposts
         Micropost.where(user_id: self.following_ids + [self.id])
+    end
+    
+    def favorite(micropost_params)
+        self.favorites.find_or_create_by(micropost_id: micropost_params.id)
+    end
+    
+    def unfavorite(micropost_params)
+        favorite = self.favorites.find_by(micropost_id: micropost_params.id)
+        favorite.destroy if favorite
+    end
+    
+    def favoriting?(micropost_params)
+        self.favoritings.include?(micropost_params)
     end
 end
